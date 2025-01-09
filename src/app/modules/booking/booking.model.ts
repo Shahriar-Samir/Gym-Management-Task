2ccd2f8bfc1c1246e28ac639bda5b8056d2c8d63
Shahriar-Samir <shabusiness035@gmail.com>
2025-01-09 13:24:09 +0600

    add: booking modle

---
+++ src/app/modules/booking/booking.model.ts
@@ -0,0 +1,32 @@
+import mongoose, { Schema, Model, model } from 'mongoose'
+import { TBooking } from './booking.interface'
+
+// Define the interface for the Booking document
+
+// Create the schema for the Booking model
+const BookingSchema = new Schema<TBooking>(
+  {
+    id: {
+      type: String,
+      required: true,
+    },
+    traineeId: {
+      type: String,
+      ref: 'Trainee', 
+      required: true,
+    },
+    scheduleId: {
+      type: String,
+      ref: 'ClassSchedule', 
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+// Create the Booking model
+const Booking = model<TBooking>('Booking', BookingSchema)
+
+export default Booking
